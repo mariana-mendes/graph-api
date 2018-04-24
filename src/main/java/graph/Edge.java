297ba5945f54e5687c70c8b5821fc26dae4f3233
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2018-04-24 16:05:53 -0300

    erro de mismatch

---
+++ src/main/java/graph/Edge.java
@@ -0,0 +1,28 @@
+package graph;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Edge {
+
+	private final static int VALUE_DEFAULT = 1;
+	
+	private Map<Integer, Map<Integer, Integer>> edge;
+
+	public Edge(int value, int vertex1, int vertex2) {
+		Map<Integer, Integer> pair = new HashMap<Integer, Integer>();
+		pair.put(vertex1, vertex2);
+		this.edge = new HashMap<Integer, Map<Integer,Integer>>();
+	    edge.put(value, pair);
+	}
+
+	public Edge(int vertex1, int vertex2) {
+		
+		Map <Integer, Integer> pair = new HashMap<Integer, Integer>();
+		pair.put(vertex1, vertex2);
+		this.edge = new HashMap<Integer, Map<Integer,Integer>>();
+	    edge.put(VALUE_DEFAULT, pair);
+	}
+
+}
--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -1,5 +1,34 @@
 package graph;
 
+import java.util.List;
+import java.util.HashMap;
+
+import java.util.Map;
+import java.util.Set;
+
 public class Graph {
+
+	private Set<Integer> vertexes;
+	private Set<Edge> edges;
+
+	public Graph() {
+
+	}
+
+	public Graph(List vertex1, List vertex2, List value, Set edges) {
+		
+		this.buildEdges(vertex1,vertex2,value);
+		
+	}
+
+	private void buildEdges(List vertex1, List vertex2, List value) {
+		for (int i = 0; i < vertex1.size(); i++) {
+			//erro object - int
+			//Edge e = new Edge(value.get(i), vertex1.get(i),vertex2.get(i));
+			//this.edges.add(e);
+		}
+		
+	}
+
 
 }
