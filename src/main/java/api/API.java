841da8d51b7d79338d082d1597aa78ca79de002e
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2018-05-11 10:57:28 -0300

    Add files via upload

--- src/main/java/api/API.java
+++ src/main/java/api/API.java
@@ -8,7 +8,11 @@ public class API {
 	
 	private GraphController controller;
 	
+	private Graph graph;
+	
 	public API() {
+		
+		this.graph = new Graph();
 		this.controller = new GraphController();
 	}
 	
@@ -29,6 +33,35 @@ public class API {
 			System.out.println(e.getMessage());
 		}
 		return null;
+	}
+	
+	public int getVertexNumber(Graph graph){
+	
+		return graph.getVertexes().size();
+		
+	}
+	
+	public int getEdgeNumber(Graph graph){
+		 return graph.getEdges().size();
+		
+	}
+	
+	
+	public float getMeanEdge(){
+		int grade = 0;
+		grade = (2*graph.getEdges().size())/graph.getVertexes().size();
+		return grade;
+		
+	}
+	
+	public void getshortestpath(int v1, int v2){
+		
+		
+	}
+    
+	private int minDistance(int[] distance, Boolean[] set) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 }
