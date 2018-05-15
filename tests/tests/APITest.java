d627c5c17597135346ccabb0407f97bbcbb0cf5c
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2018-05-14 23:01:50 -0300

    testesss

--- tests/tests/APITest.java
+++ tests/tests/APITest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Set;
 import org.junit.Assert;
 import org.junit.Before;
@@ -16,7 +18,7 @@ public class APITest {
 	@Before
 	public void TestAPI() {
 		api = new API();
-		graph = new Graph();
+		graph = api.readGraph("input.txt");
 	}
 
 	@Test
@@ -29,10 +31,10 @@ public class APITest {
 		}
 
 		Set<Edge> e = graph.getEdges();
-		for(Edge ee :e) {
+		for (Edge ee : e) {
 			System.out.println(ee.getEdge());
 		}
-		
+
 		System.out.println(api.graphRepresentation(graph, "AM"));
 	}
 
@@ -58,10 +60,44 @@ public class APITest {
 	@Test
 	public void testGetMeanEdge() {
 
-		graph = api.readGraph("input.txt");
+		// graph = api.readGraph("input.txt");
 		Assert.assertEquals(2, api.getMeanEdge(graph), 0);
 		Assert.assertNotEquals(0, api.getMeanEdge(graph));
 		Assert.assertNotEquals(5, api.getMeanEdge(graph));
+	}
+
+	@Test
+	public void testGraphRepresentationList() {
+		// // 1 2
+		//
+		// // 2 5 2
+		// // 5 3 / \
+		// 1---5
+		// // 4 5 / \
+		// 1 5 4 3
+		String list = System.lineSeparator();
+		list += "1 - 5 2" + System.lineSeparator();
+		list += "2 - 1 5" + System.lineSeparator();
+		list += "3 - 5" + System.lineSeparator();
+		list += "4 - 5" + System.lineSeparator();
+		list += "5 - 1 2 4 3";
+		System.out.println(api.graphRepresentation(graph, "AL"));
+		assertEquals(list, api.graphRepresentation(graph, "AL"));
+	}
+
+	@Test
+	public void testBFS() {
+	//		 2     -------  0
+	//      / \
+	//     1---5   -------  1
+	//        / \
+	//       4   3 -------  2
+	String bfsResult = "2 - 0 - " +System.lineSeparator()+
+					   "1 - 1 - 2" + System.lineSeparator()+
+					   "5 - 1 - 2" + System.lineSeparator()+
+					   "4 - 2 - 5" + System.lineSeparator()+
+					   "3 - 2 - 5" + System.lineSeparator();
+	assertEquals(bfsResult, api.BFS(graph, 2));
 	}
 
 }
